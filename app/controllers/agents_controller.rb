896c67c0019794c4160ba2b2e892f2f9a2ec6165
Aniket239 <aniketbiswas2392001@gmail.com>
2024-03-30 12:46:56 +0530

    agents

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -2,14 +2,12 @@ class AgentsController < ApplicationController
     def allAgents
         client_id = '1000.RMODJ3TXVWLVGROZQR2CYKWAQQL4RK'
         client_secret = '7241a1ead9a8513ebea78500298e54fb2db44cee9d'
-        refresh_token = cookies[:refresh_token]
         agents_url = "https://desk.zoho.in/api/v1/agents"
         access_token = cookies[:access_token]
         response = HTTParty.get(agents_url, headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" })
-        agents = response.parsed_response
+        p agents = response.parsed_response
         p "========================= agents ========================"
         return agents
-        # redirect_to tickets_index_path
     end
 
     def login
