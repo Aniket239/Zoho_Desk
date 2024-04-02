cd0c3b98519c6b59a4563f34a1994abff41cd896
Aniket239 <aniketbiswas2392001@gmail.com>
2024-04-02 19:39:25 +0530

    mail

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,6 +1,7 @@
 class WebhooksController < ApplicationController
     skip_before_action :verify_authenticity_token
     require 'mail'
+
     def receive
       request_data = params
       if request_data.dig('_json', 0, 'eventType') == 'Ticket_Update'
@@ -44,11 +45,10 @@ class WebhooksController < ApplicationController
         client_secret: client_secret,
         grant_type: 'refresh_token'
       })
-  
+
       if response.code == 200
         access_token = response.parsed_response['access_token']
         threads_response = HTTParty.get("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/threads", headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" })
-  
         contents = []
         threads_response["data"].each do |thread|
           thread_id = thread["id"]
@@ -61,9 +61,42 @@ class WebhooksController < ApplicationController
           p "============================ index ====================================="
           p charset_index = formatted_content.rindex('charset="UTF-8"')
           p "============================ cut string ====================================="
+          replacements = {
+          "charset=\"UTF-8\"" => "",  # Replace with an empty string
+          "=\\" => "", 
+          "3D\"\"" => " ",
+          "=20" => " ",
+          "=C2=A0" => " ",
+          "=E2=80=AF" => " ",
+          "=E2=80=99" => "'",
+          "=E2=80=9D" => '"',
+          "=E2=80=98" => "'",
+          "=E2=80=9C" => '"',
+          "=E2=80=9E" => '"',
+          "=E2=80=9F" => '"',
+          "=E2=80=9A" => "'",
+          "=E2=80=9B" => "'",
+          "=E2=80=B3" => "'",
+          "=E2=80=B2" => "'",
+          "=E2=80=B4" => "'",
+          "=E2=80=B5" => "'",
+          "=E2=80=B6" => "'",
+          "=E2=80=B7" => "'",
+          "=E2=80=B8" => "'",
+          "=E2=80=B9" => "'",
+          "=E2=80=BA" => "'",
+          "=E2=80=BB" => "'",
+          "=E2=80=BC" => "'",
+          "=E2=80=BD" => "'",
+          "=E2=80=BE" => "'",
+          "=E2=80=BF" => "'",
+        }
           p cut_string = formatted_content.slice(charset_index,formatted_content.length) if charset_index
-          p "============================ index ====================================="
-        UserMailer.testEmail(cut_string, subject).deliver_now
+          p "============================ proper string ====================================="
+          p proper_string = cut_string.gsub(Regexp.union(replacements.keys), replacements)
+          cleaned_string = proper_string.gsub(/= +/, '')
+          p "============================ proper string ====================================="
+        UserMailer.testEmail(cleaned_string, subject).deliver_now
       else
         p "Failed to refresh token"
       end
