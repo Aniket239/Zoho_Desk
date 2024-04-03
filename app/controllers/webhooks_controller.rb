84fe04d13b93337d42e8a201dca038b4e2a99959
Aniket239 <aniketbiswas2392001@gmail.com>
2024-04-03 17:17:11 +0530

    mail

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -17,8 +17,8 @@ class WebhooksController < ApplicationController
   
     def process_ticket_update(event)
       refresh_token='1000.2b9b9cbdcdadc05749f6d17f19c18ede.7f0255d317ca218bf5c1c56e8d4cdbad'
-      p "Processing Ticket Update Event"
       payload = event['payload'] || {}
+      payload
       ticket_number = payload['ticketNumber']
       ticket_id = payload['id']
       ticket_status = payload['status']
@@ -48,37 +48,32 @@ class WebhooksController < ApplicationController
 
       if response.code == 200
         access_token = response.parsed_response['access_token']
-        # threads_response = HTTParty.get("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/threads", headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" })
-        # contents = []
-        # content = ""
-        # threads_response["data"].each do |thread|
-        #   thread_id = thread["id"]
-        #   content_response = HTTParty.get("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/threads/#{thread_id}/originalContent", headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" })
-        #   p "=======================================================content ============================================="
-        #   p content = content_response.parsed_response["content"]
-        #   p "=======================================================content ============================================="
-        #   contents << content
-        # end
-        #   # decoded_content = Mail::Encodings::QuotedPrintable.decode(contents[0])
-        #   # formatted_content = decoded_content.gsub(/=\r\n/, '').encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
-        #   # p "============================ index ====================================="
-        #   # p charset_index = formatted_content.rindex('charset="UTF-8"')
-        #   # p "============================ cut string ====================================="
-        #   # replacements = {"Content-Transfer-Encoding: quoted-printable"=>"","charset=\"UTF-8\"" => "","3D\"\"" => " ","=20" => " ","=C2=A0" => " ","=E2=80=AF" => " ","=E2=80=99" => "'","=E2=80=9D" => '"',"=E2=80=98" => "'","=E2=80=9C" => '"',"=E2=80=9E" => '"',"=E2=80=9F" => '"',"=E2=80=9A" => "'","=E2=80=9B" => "'","=E2=80=B3" => "'","=E2=80=B2" => "'","=E2=80=B4" => "'","=E2=80=B5" => "'","=E2=80=B6" => "'","=E2=80=B7" => "'","=E2=80=B8" => "'","=E2=80=B9" => "'","=E2=80=BA" => "'","=E2=80=BB" => "'","=E2=80=BC" => "'","=E2=80=BD" => "'","=E2=80=BE" => "'","=E2=80=BF" => "'","=C2=B7" => ""}
-        #   # p cut_string = formatted_content.slice(charset_index,formatted_content.length) if charset_index
-        #   # p "============================ proper string ====================================="
-        #   # p proper_string = cut_string.gsub(Regexp.union(replacements.keys), replacements)
-        #   # p "============================ proper string ====================================="
-        #   p "=================================== mail ==========================================="
-        #   p original_mail = Mail.read_from_string(content)
-        #   p "=================================== mail ==========================================="
-        # UserMailer.testEmail(original_mail,subject).deliver_now
-        p threads_response = HTTParty.get("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/sendReply", headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" },body:{
-          to : "system4@thejaingroup.com",
-          contentType : "html",
-          isForward : true
-        })
-
+        if access_token
+          threads_response = HTTParty.get("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/threads",headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" })
+          p "=========================================== thread id ========================================"
+          p threads_response_content = threads_response.parsed_response
+          thread_id = threads_response_content["data"][0]["id"]
+          threads_content = HTTParty.get("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/threads/#{thread_id}/originalContent",headers: { 'Authorization' => "Zoho-oauthtoken #{access_token}" })
+          p "=========================================== thread id ========================================"
+          forward_response = HTTParty.post("https://desk.zoho.in/api/v1/tickets/#{ticket_id}/sendReply", body: {
+            channel: "EMAIL",
+            isForward: 'true',
+            fromEmailAddress: "support@thejaingroup.zohodesk.in",
+            to: 'system4@thejaingroup.com',  # The recipient's email address
+            content: threads_content["content"],
+            # ,142998000000299471
+            contentType: 'html'  # or 'plainText' if applicable
+          }.to_json, headers: {
+            "Authorization" => "Zoho-oauthtoken #{access_token}",
+          })
+      
+          if forward_response.code == 200
+            p "Ticket forwarded successfully"
+          else
+            p "Failed to forward ticket"
+            p forward_response.parsed_response  # To understand what went wrong
+          end
+        end
       else
         p "Failed to refresh token"
       end
