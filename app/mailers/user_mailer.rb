335210209632b148b1d82a3a86df6e2fa41df7f3
Aniket239 <aniketbiswas2392001@gmail.com>
2024-04-08 18:39:51 +0530

    webhook

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,8 @@ class UserMailer < ApplicationMailer
     def testEmail(content_mail, subject,recipient_email,author)
       @content = content_mail
       @author = author
-      mail(to: recipient_email, subject: subject, bcc: "system4@thejaingroup.com") do |format|
+      mail(to: recipient_email, subject: subject,  cc: "rishi@thejaingroup.com", bcc: "system4@thejaingroup.com") do |format|
         format.html { render html: content_mail.html_safe }
       end
     end
   end
-  # cc: "rishi@thejaingroup.com",
