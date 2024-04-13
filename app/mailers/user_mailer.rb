9b3bd091491628453e11e00359553343509ba3ef
Aniket239 <aniketbiswas2392001@gmail.com>
2024-04-13 16:03:31 +0530

    webhook

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,18 +3,13 @@ class UserMailer < ApplicationMailer
   layout 'mailer'
     def zohoMail(content_mail, subject,recipient_email,agent_name,note,assigneer_email,cc)
       @content = content_mail
-      p "================================================ agent_name ========================================= "
       @agent_name = agent_name
-      p @agent_name
-      p "================================================ note ========================================= "
       @note = note
-      p @note
       if cc=="true"
         cc_mail=  "rishi@thejaingroup.com"
       else 
         cc_mail=  nil
       end
-      p cc_mail
       mail(to: recipient_email, subject: subject, cc: [cc_mail,assigneer_email], bcc: "system4@thejaingroup.com")
     end
   end
